e889d595fd229da633023d685ea43ce7eaf37dbc
David Zapico <davidzapico@davidzapico.com>
2021-11-03 18:44:50 +0100

    3/11/21

--- tema3/practica2/ej9.php
+++ tema3/practica2/ej9.php
@@ -1,24 +1,20 @@
 <?php
-$ran = array(1,2,3,4); 
-$randomElement = $ran[array_rand($ran, 1)];
-
-echo $randomElement;
-
-
-//Metodo para buscar el valor maximo de toda la matriz y coger su posicion en la fila y en la columna.
-/*public static void maxMatriz(int [][] matriz) {
-    int max = 0, filamax = 0, colmax = 0, col, fila;
-    
-    for (col = 0; col < 5; col++) {
-        for (fila = 0; fila < 5; fila++) {
-            if (matriz[fila][col] > max) {
-                max = matriz[fila][col];
-                filamax = fila;
-                colmax = col;
+$matriz = array(
+    array(1, 14, 8, 3),
+    array(6, 19, 7, 2),
+    array(3, 13, 4, 1),
+);
+   
+    for ($col = 0; $col < 4; $col++) {
+        for ($fila = 0; $fila < 3; $fila++) {
+            if ($matriz[$fila][$col] > $max) {
+                $max = $matriz[$fila][$col];
+                $filamax = $fila;
+                $colmax = $col;
             }
         }
     }
-    System.out.println("El valor maximo de la matriz es: " +max+ " y su posicion es, en la fila: " +filamax+ " y en la columna: " +colmax);
-}*/
+    echo"El valor maximo de la matriz es: $max y su posicion es, en la fila: $filamax y en la columna: $colmax";
+}
 
 ?>
