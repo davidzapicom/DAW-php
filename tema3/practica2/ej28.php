761d4c8f35233263bbbf6942f6a725f323147561
DESKTOP-MH6J3A5\admin <davidzapico@davidzapico.com>
2021-11-11 09:41:41 +0100

    11/11/21

--- tema3/practica2/ej28.php
+++ tema3/practica2/ej28.php
@@ -1,3 +1,29 @@
 <?php
+$datos = array(
+    array("Familia" => "Simpson", 
+    "Padre" => "Hommer", 
+    "Madre" => "Marge", 
+    "Hijos" => array("Bart","Lisa","Maggie"),
+    ),
 
+    array("Familia" => "Griffin", 
+    "Padre" => "Peter", 
+    "Madre" => "Lois", 
+    "Hijos" => array("Chris","Meg","Stewie"),
+    )
+);
+
+echo "<ul>";
+foreach ($datos as $indice => $valor){
+    foreach ($valor as $indice2 => $valor2){
+        if ($indice2 == "Hijos"){
+            foreach ($valor2 as $indice => $valor3){
+                echo "<li> $valor3 </li>";
+            }
+        } else {
+            echo "<li> $indice2-> $valor2 </li>";
+        }
+    }
+}
+echo "</ul>";
 ?>
