9d92ac6aa8a4413ac29f2d34fbb16b086bd4e382
David Zapico <davidzapico@davidzapico.com>
2021-11-14 13:19:00 +0100

    14/11/21

--- tema3/practica3/ej5.php
+++
@@ -1,5 +0,0 @@
-<input type="radio" name="sexo" <?php if (isset($sexo) &&
-$sexo=="mujer") echo "checked";?> value="mujer"> Mujer
-<input type="radio" name="sexo" <?php if (isset($sexo) &&
-$sexo=="hombre") echo "checked";?>value="hombre"> Hombre
-<span class="error">* <?php echo $sexoErr;?></span><br><br>
--- tema3/practica3/ej7.php
+++
@@ -1,2 +0,0 @@
-mail: <input type="text" name="email" value="<?php echo $email;?
->"><span class="error">* <?php echo $emailErr;?></span><br><br>
