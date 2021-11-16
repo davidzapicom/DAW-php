95f18d842f95573119efdc9957606cff900a7f1a
DESKTOP-MH6J3A5\admin <davidzapico@davidzapico.com>
2021-11-16 10:34:00 +0100

    16/11/21

--- tema3/practica6/confirmacion.php
+++ tema3/practica6/confirmacion.php
@@ -1,5 +1,26 @@
-<?php
-$nom = $_POST['nombre'];
-$pass = $_POST['contraseña'];
-echo "<p>El usuario se llama $nom $pass </p>";
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        fieldset {
+            display: inline-block;
+            border: 3px solid black;
+        }
+    </style>
+</head>
+<body>
+    <fieldset>
+        <p>Los datos introducidos son los siguientes.</p>
+        <?php
+        $nom = $_POST['nombre'];
+        $pass = $_POST['contraseña'];
+        echo "<p>Usuario: $nom <br/> Contraseña: $pass</p>";
+        ?>
+    </fieldset>
+</body>
+</html>
