f3d7ac609e6f1da836a3857850ec83721b7bddd2
DESKTOP-MH6J3A5\admin <davidzapico@davidzapico.com>
2021-12-03 13:27:07 +0100

    03

---
+++ tema3/practica10/Ej1/recarga.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($_POST['anadir'])) {
+    $_SESSION['productos'][$_POST['anadir']]["cantidad"]++; 
+}
+
+if (isset($_POST['quitar'])) {
+    if ($_SESSION['productos'][$_POST['quitar']]["cantidad"] != 0) {
+        $_SESSION['productos'][$_POST['quitar']]["cantidad"]--; 
+    }
+}
+
+?>
