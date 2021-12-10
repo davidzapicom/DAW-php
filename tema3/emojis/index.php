707eb40a82ea6bce5413ab242d66487d981b7a46
DESKTOP-MH6J3A5\admin <davidzapico@davidzapico.com>
2021-12-10 12:51:47 +0100

    10

---
+++ tema3/emojis/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        * {
+          padding: 0;
+          margin: 0;
+        }
+
+        table {
+          margin: 20px auto;
+          border: 1px solid black;
+        }
+
+        tr,td {
+          border: 1px solid black;
+        }
+
+        img {
+
+        }
+
+        button {
+          padding: 10px;
+          border: 1px solid black;
+          text-transform: uppercase;
+          margin: 20px;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <tr>
+          <td><img src="img/cool.png" width="35%" alt="cool"></td>
+          <td><img src="img/crazy.png" width="30%" alt="crazy"></td>
+          <td><img src="img/shit.png" width="10%" alt="shit"></td>
+        </tr>
+        <tr>
+          <td><img src="img/laugh.png" width="30%" alt="laugh"></td>
+          <td><img src="img/smile.png" width="30%" alt="smile"></td>
+          <td><img src="img/think.png" width="10%" alt="think"></td>
+        </tr>
+      </table>
+      <br/>
+      <form action="#" method="POST">
+        <input type="submit" name="boton" value="Barajar">
+      </form>
+<?php
+
+
+?>
+</body>
+</html>
