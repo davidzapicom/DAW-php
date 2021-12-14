17c59d8a9d2ca2ef215c6e6f162352f2376601f5
David Zapico <davidzapico@davidzapico.com>
2021-12-14 09:42:34 +0000

    Update tragaperras2.php

--- examenes/1eval/Ej2/tragaperras2.php
+++ examenes/1eval/Ej2/tragaperras2.php
@@ -57,59 +57,6 @@ if ($accion == "jugar" && $_SESSION["monedas"] > 0) {
         ($_SESSION["fruta2"] == $cereza && $_SESSION["fruta3"] == $cereza) ||
         ($_SESSION["fruta1"] == $cereza && $_SESSION["fruta3"] == $cereza)) {
         $_SESSION["premio"] += 2;
-    } elseif ($_SESSION["fruta1"] == $piña && $_SESSION["fruta2"] == $piña && $_SESSION["fruta3"] == $piña) {
-        $_SESSION["premio"] += 5;
-    
-    } elseif (($_SESSION["fruta1"] == $piña && $_SESSION["fruta2"] == $piña) ||
-        ($_SESSION["fruta2"] == $piña && $_SESSION["fruta3"] == $piña) ||
-        ($_SESSION["fruta1"] == $piña && $_SESSION["fruta3"] == $piña)) {
-        $_SESSION["premio"] += 2;
-    } elseif ($_SESSION["fruta1"] == $limon && $_SESSION["fruta2"] == $limon && $_SESSION["fruta3"] == $limon) {
-        $_SESSION["premio"] += 5;
-    
-    } elseif (($_SESSION["fruta1"] == $limon && $_SESSION["fruta2"] == $limon) ||
-        ($_SESSION["fruta2"] == $limon && $_SESSION["fruta3"] == $limon) ||
-        ($_SESSION["fruta1"] == $limon && $_SESSION["fruta3"] == $limon)) {
-        $_SESSION["premio"] += 2;
-    } elseif ($_SESSION["fruta1"] == $fresa && $_SESSION["fruta2"] == $fresa && $_SESSION["fruta3"] == $fresa) {
-        $_SESSION["premio"] += 5;
-    
-    } elseif (($_SESSION["fruta1"] == $fresa && $_SESSION["fruta2"] == $fresa) ||
-        ($_SESSION["fruta2"] == $fresa && $_SESSION["fruta3"] == $fresa) ||
-        ($_SESSION["fruta1"] == $fresa && $_SESSION["fruta3"] == $fresa)) {
-        $_SESSION["premio"] += 2;
-
-    } elseif ($_SESSION["fruta1"] == $platano && $_SESSION["fruta2"] == $platano && $_SESSION["fruta3"] == $platano) {
-        $_SESSION["premio"] += 5;
-    
-    } elseif (($_SESSION["fruta1"] == $platano && $_SESSION["fruta2"] == $platano) ||
-        ($_SESSION["fruta2"] == $platano && $_SESSION["fruta3"] == $platano) ||
-        ($_SESSION["fruta1"] == $platano && $_SESSION["fruta3"] == $platano)) {
-        $_SESSION["premio"] += 2;
-
-    } elseif ($_SESSION["fruta1"] == $naranja && $_SESSION["fruta2"] == $naranja && $_SESSION["fruta3"] == $naranja) {
-        $_SESSION["premio"] += 5;
-    
-    } elseif (($_SESSION["fruta1"] == $naranja && $_SESSION["fruta2"] == $naranja) ||
-        ($_SESSION["fruta2"] == $naranja && $_SESSION["fruta3"] == $naranja) ||
-        ($_SESSION["fruta1"] == $naranja && $_SESSION["fruta3"] == $naranja)) {
-        $_SESSION["premio"] += 2;
-
-    } elseif ($_SESSION["fruta1"] == $sandia && $_SESSION["fruta2"] == $sandia && $_SESSION["fruta3"] == $sandia) {
-        $_SESSION["premio"] += 5;
-    
-    } elseif (($_SESSION["fruta1"] == $sandia && $_SESSION["fruta2"] == $sandia) ||
-        ($_SESSION["fruta2"] == $sandia && $_SESSION["fruta3"] == $sandia) ||
-        ($_SESSION["fruta1"] == $sandia && $_SESSION["fruta3"] == $sandia)) {
-        $_SESSION["premio"] += 2;
-
-    } elseif ($_SESSION["fruta1"] == $uvas && $_SESSION["fruta2"] == $uvas && $_SESSION["fruta3"] == $uvas) {
-        $_SESSION["premio"] += 5;
-    
-    } elseif (($_SESSION["fruta1"] == $uvas && $_SESSION["fruta2"] == $uvas) ||
-        ($_SESSION["fruta2"] == $uvasuvas && $_SESSION["fruta3"] == $uvas) ||
-        ($_SESSION["fruta1"] == $uvas && $_SESSION["fruta3"] == $uvas)) {
-        $_SESSION["premio"] += 2;
     } else {
         $_SESSION["premio"] += 0;
     }
