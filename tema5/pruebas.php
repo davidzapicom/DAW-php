7b5632107314be031dfd909a9d65c6253631ad8f
davidzapicom <davidzapico@davidzapico.com>
2021-12-16 11:19:59 +0100

    16

--- tema5/pruebas.php
+++ tema5/pruebas.php
@@ -1,8 +1,7 @@
 <?php
 $enlace = mysqli_connect('localhost','root','','control-usuarios');
 
-mysqli_query($enlace, "INSERT INTO usuario (DNI, nombre, apellidos, password)
-VALUES ('55555555E', 'JOSE ANTONIO', 'ALVAREZ GARCIA', 'trhsh3@')");
+mysqli_query($enlace, "UPDATE usuario SET apellidos='*' WHERE nombre='ERDANI'");
 
 mysqli_close($enlace);
 ?>
