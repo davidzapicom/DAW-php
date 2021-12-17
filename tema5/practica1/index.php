c0cdce4c67b8844dfbf9e5d3321d605463bce41f
davidzapicom <davidzapico@davidzapico.com>
2021-12-17 14:27:56 +0100

    17

---
+++ tema5/practica1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+</head>
+<body>
+    <form action="#" method="post">
+        Usuarie: <input type="text" name="nombre"> <br/>
+        Contraseña: <input type="password" name="password">
+        <input type="submit" name="boton" value="Ingresar">
+    </form>
+</body>
+</html>
+
+
+<?php
+
+
+?>
