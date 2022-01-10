925befd7395abd7e9b941949aaa601243e2b7425
davidzapicom <davidzapico@davidzapico.com>
2022-01-10 14:16:50 +0100

    10

--- tema5/practica1/index.php
+++ tema5/practica1/index.php
@@ -9,14 +9,36 @@
     </head>
     <body>
     <div class="contenedor">
-        <h2>LOGIN</h2>
-        <form method="post" action="#">  
-            Usuari@: <input type="text" name="namusue" value="">
-            <br/><br/>
-            Clave: <input type="password" name="pass" value="">
-            <br/><br/>
-            <input type="submit" name="login" value="Iniciar sesion">  
-        </form>
+      <form action="#" method="post">
+        <h1>Log In</h1>
+        <input type="text" name="usu" placeholder="Usuari@">
+        <input type="password" name="pass" placeholder="Contraseña">
+        <input type="submit" name="login" value="Iniciar sesion">
+      </form>
     </div>
     </body>
+<?php
+session_start();
+  $usu = $_SESSION["usu"] = $_POST["usu"];
+  $pass = $_SESSION["pass"] = $_POST["pass"];
+session_destroy();
+
+$sentencia = "SELECT usuario, password, rol FROM usuarios WHERE usuario='$usuario' AND password='$pass'";
+$enlace = mysqli_connect("localhost", "root", "", "ventas");
+$consulta = mysqli_query($enlace, $sentencia);
+$fetch = mysqli_fetch_assoc($consulta);
+mysqli_close($enlace);
+
+if (mysqli_nums_rows($consulta) == 0) {
+    echo "Este usuario no existe";
+} else {
+  $enlace = mysqli_connect("localhost", "root", "", "ventas");
+  if ($fetch["rol"] == "consultor") {
+    $rolusu = "consultor";
+  } else {
+    $rolusu = "administrador";
+  }
+  echo "Hola $usuario, eres <p class='up'> $rolusu </p>";
+}
+?>
 </html>
