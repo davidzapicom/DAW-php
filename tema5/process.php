3006d7b72ab0d55dc8bb7bfff3731ae0d613a893
davidzapicom <davidzapico@davidzapico.com>
2022-01-13 09:38:24 +0100

    13

--- tema5/process.php
+++ tema5/process.php
@@ -1,12 +1,12 @@
 <?php 
 session_start();
-$name = $_POST['Name'];
-$pass = $_POST['Password'];
+$name = $_SESSION['Name'] = $_POST['Name'];
+$pass = $_SESSION['Password'] = $_POST['Password'];
     if(isset($_POST['Login'])) {
         if(empty($_POST['Name']) || empty($_POST['Password'])) {
             header("location:index.php");
         } else {
-            $sentencia="SELECT * FROM usuarios where usuario='".$_POST['Name']."' and password='".$_POST['Password']."'";
+            $sentencia="SELECT * FROM usuarios where usuario='$name' and password='$pass'";
             $con=mysqli_connect('localhost','root','','ventas');
             $result=mysqli_query($con,$sentencia);
             $fetch = mysqli_fetch_assoc($result);
@@ -15,11 +15,11 @@ $pass = $_POST['Password'];
                 echo "Este usario no existe";
             } else {
                 if ($fetch['rol'] == 'consultor') {
-                    $con=mysqli_connect('localhost','consultor','consultor','ventas');ç
-                    
+                    $con=mysqli_connect('localhost','consultor','consultor','ventas');
                 } else {
                     $con=mysqli_connect('localhost','administrador','administrador','ventas');
                 }
+                header("location:wellcome.php");
             echo 'Hola ' .$name. ', eres ' .$fetch['rol']. ".";
         }
     }
