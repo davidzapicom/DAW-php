d05a40eb59b43385a2adf324da10331c3590ac04
davidzapicom <davidzapico@davidzapico.com>
2022-01-29 12:09:12 +0100

    29

--- tema6/t06-errores/die.php
+++ tema6/t06-errores/die.php
@@ -1,18 +1,4 @@
 <?php
-
-
-$vecx = [];
 $sol = 9/0;
-echo $sol1;
-if($vec[5])
-	print_r($vec[5]);
-else
-	die("<p>No puedes acceder a esa posicion, ¡no existe!</p>");
-
-    if(funci())
-	print_r(funci());
-else
-	die("<p>No puedes acceder a esa funcion, ¡no existe!</p>");
-
-
+echo $sol;
 ?>
