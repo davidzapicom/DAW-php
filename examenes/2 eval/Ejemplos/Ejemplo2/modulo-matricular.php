e20edf9154a89afd4624712a6522150b9f5d0a7a
David Zapico <davidzapico@davidzapico.com>
2022-02-19 15:39:04 +0100

    19

--- examenes/2 eval/Ejemplos/Ejemplo2/modulo-matricular.php
+++ examenes/2 eval/Ejemplos/Ejemplo2/modulo-matricular.php
@@ -28,28 +28,27 @@
     session_start();
     $dni = $_SESSION["dni"];
     $usu = $_SESSION["usuario"];
-    $tipo = $_POST['tipo'];
+    $tipo = $_SESSION['tipo'] = $_POST['tipo'];
     $inscripcion = date('Y-m-d');
     $codcurso = $_SESSION["codcurso"] = $_POST["codcurso"];
-    $pruebaA = $_POST["pruebaA"];
-    $pruebaB = $_POST["pruebaB"];
+    $pruebaA = $_SESSION["pruebaA"] = $_POST["pruebaA"];
+    $pruebaB = $_SESSION["pruebaB"] = $_POST["pruebaB"];
     echo '<p>Alumno: ' . $dni . ' <br/> Nombre: ' . $usu . '</p>';
     
-
-
+    /*------------------------------------------------------------------------------*/
     if (isset($_POST["guardar"])) {
         $con = mysqli_connect('localhost', 'root', '', 'oposicion');
         $select = "SELECT * FROM curso WHERE codigocurso= '$codcurso'";
         $resultado = mysqli_query($con, $select);
         if (mysqli_num_rows($resultado) != 0) {
             $introducir = "INSERT INTO matricula (dnialumno, codcurso, pruebaA, pruebaB, tipo, inscripcion) VALUES ('$dni', '$codcurso', '$pruebaA', '$pruebaB', '$tipo', '$inscripcion')";
-            $introducirCompra = mysqli_query($con, $introducir);
-
-
+            $introducirMatricula = mysqli_query($con, $introducir);
+            echo 'Matricula del alumno ' .$dni. ' en el curso ' .$codcurso. ' introducida correctamente!';
         } else {
-            echo 'No se ha podido introducir la matricula del alumno ' .$dni. ' en el curso ' .$codcurso. '.';
+            echo 'No se ha podido introducir la matricula del alumno ' .$dni. ' en el curso ' .$codcurso. ', código de curso incorrecto.';
+            $_SESSION['codcurso'] = " ";
         }
-    } else {
+    }
     ?>
         <h2>Matriculación</h2>
         <p><span class="error">* Campos requeridos.</span></p>
@@ -57,18 +56,18 @@
             DNI: <input type="text" name="dni" value="<?php echo $dni; ?>" required>
             <span class="error">*</span>
             <br><br>
-            COD CURSO: <input type="text" name="codcurso" value="<?php echo $codcurso; ?>" required>
+            COD CURSO: <input type="text" name="codcurso" value="<?php echo $_SESSION['codcurso']; ?>" required>
             <span class="error">*</span>
             <br><br>
-            PRUEBA A: <input type="number" name="pruebaA" value="<?php echo $pruebaA; ?>" required>
+            PRUEBA A: <input type="number" name="pruebaA" value="<?php echo $_SESSION['pruebaA']; ?>" required>
             <span class="error">*</span>
             <br><br>
-            PRUEBA B: <input type="number" name="pruebaB" value="<?php echo $pruebaB; ?>" required>
+            PRUEBA B: <input type="number" name="pruebaB" value="<?php echo $_SESSION['pruebaB']; ?>" required>
             <span class="error">*</span>
             <br><br>
             TIPO:
-            <input type="radio" name="tipo" value="Oficial" value="<?php echo $tipo; ?>" required>Oficial
-            <input type="radio" name="tipo" value="Libre" value="<?php echo $tipo; ?>" required>Libre 
+            <input type="radio" name="tipo" value="Oficial <?php echo $_SESSION['tipo']; ?>" required>Oficial
+            <input type="radio" name="tipo" value="Libre <?php echo $_SESSION['tipo']; ?>" required>Libre 
             <span class="error">*</span>
             <br><br>
             INSCRIPCION: <input type="date" name="inscripcion" value="<?php echo $inscripcion; ?>" max="<?php echo $inscripcion; ?>" required>
@@ -76,8 +75,5 @@
             <br><br>
             <input type="submit" value="Guardar" name="guardar" />
         </form>
-    <?php
-    }
-    ?>
 </body>
 </html>
