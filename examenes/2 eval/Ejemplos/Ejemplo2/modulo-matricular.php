b3f59f1dcfed00c30e984c61205e3fa544abdab3
David Zapico <davidzapico@davidzapico.com>
2022-02-19 15:18:38 +0100

    19

--- examenes/2 eval/Ejemplos/Ejemplo2/modulo-matricular.php
+++ examenes/2 eval/Ejemplos/Ejemplo2/modulo-matricular.php
@@ -1,10 +1,13 @@
 <!DOCTYPE HTML>
 <html>
-
 <head>
     <style>
         .error {
             color: #FF0000;
+        }
+
+        p {
+            text-transform: uppercase;
         }
 
         /* Chrome, Safari, Edge, Opera */
@@ -20,109 +23,56 @@
         }
     </style>
 </head>
-
 <body>
-
     <?php
     session_start();
     $dni = $_SESSION["dni"];
     $usu = $_SESSION["usuario"];
+    $tipo = $_POST['tipo'];
+    $inscripcion = date('Y-m-d');
+    $codcurso = $_SESSION["codcurso"] = $_POST["codcurso"];
+    $pruebaA = $_POST["pruebaA"];
+    $pruebaB = $_POST["pruebaB"];
     echo '<p>Alumno: ' . $dni . ' <br/> Nombre: ' . $usu . '</p>';
-
-    // define variables and set to empty values
-    $nameErr = $emailErr = $genderErr = $websiteErr = "";
-    $name = $email = $gender = $comment = $website = "";
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST["name"])) {
-            $nameErr = "Nombre requerido";
-        } else {
-            $name = limpiar_datos($_POST["name"]);
-            // comprobar si el nombre contiene solo letras y espacios en blanco.
-            if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
-                $nameErr = "Solo se aceptan letras y espacios en blanco.";
-            }
-        }
-
-        if (empty($_POST["email"])) {
-            $emailErr = "Email requerido";
-        } else {
-            $email = limpiar_datos($_POST["email"]);
-            // comprobar si la direccion email es correcta.
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $emailErr = "Dirección de correo invalida.";
-            }
-        }
-
-        if (empty($_POST["website"])) {
-            $website = "";
-        } else {
-            $website = limpiar_datos($_POST["website"]);
-            // comprobar si la direccion URL es valida.
-            if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $website)) {
-                $websiteErr = "URL no válida.";
-            }
-        }
-
-        if (empty($_POST["comment"])) {
-            $comment = "";
-        } else {
-            $comment = limpiar_datos($_POST["comment"]);
-        }
-
-        if (empty($_POST["gender"])) {
-            $genderErr = "Gender is required";
-        } else {
-            $gender = limpiar_datos($_POST["gender"]);
-        }
-    }
-
-    function limpiar_datos($data)
-    {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-
-
+    
 
 
     if (isset($_POST["guardar"])) {
-        $enlace = mysqli_connect('localhost', 'root', '', 'oposicion');
-        $select = "SELECT * FROM alumno WHERE dniA= '$dni'";
-        $resultado = mysqli_query($enlace, $select);
+        $con = mysqli_connect('localhost', 'root', '', 'oposicion');
+        $select = "SELECT * FROM curso WHERE codigocurso= '$codcurso'";
+        $resultado = mysqli_query($con, $select);
         if (mysqli_num_rows($resultado) != 0) {
-
-
-
+            $introducir = "INSERT INTO matricula (dnialumno, codcurso, pruebaA, pruebaB, tipo, inscripcion) VALUES ('$dni', '$codcurso', '$pruebaA', '$pruebaB', '$tipo', '$inscripcion')";
+            $introducirCompra = mysqli_query($con, $introducir);
 
 
         } else {
-            echo 'La matricula del alumno ' .$dni. ' en el curso 0001 se ha realizado correctamente';
+            echo 'No se ha podido introducir la matricula del alumno ' .$dni. ' en el curso ' .$codcurso. '.';
         }
     } else {
     ?>
         <h2>Matriculación</h2>
         <p><span class="error">* Campos requeridos.</span></p>
         <form action="#" method="post">
-            DNI: <input type="text" name="dni" value="<?php echo $dni; ?>" require>
-            <span class="error">* <?php echo $dniErr; ?></span>
+            DNI: <input type="text" name="dni" value="<?php echo $dni; ?>" required>
+            <span class="error">*</span>
             <br><br>
-            COD CURSO: <input type="text" name="codcurso" value="<?php echo $codcurso; ?>" require>
-            <span class="error">* <?php echo $codcursoErr; ?></span>
+            COD CURSO: <input type="text" name="codcurso" value="<?php echo $codcurso; ?>" required>
+            <span class="error">*</span>
             <br><br>
-            PRUEBA A: <input type="text" name="pruebaA" value="<?php echo $pruebaA; ?>" require>
-            <span class="error">* <?php echo $pruebaAErr; ?></span>
+            PRUEBA A: <input type="number" name="pruebaA" value="<?php echo $pruebaA; ?>" required>
+            <span class="error">*</span>
             <br><br>
-            PRUEBA B: <input type="text" name="pruebaB" value="<?php echo $pruebaB; ?>" require>
-            <span class="error">* <?php echo $pruebaBErr; ?></span>
+            PRUEBA B: <input type="number" name="pruebaB" value="<?php echo $pruebaB; ?>" required>
+            <span class="error">*</span>
             <br><br>
-            TIPO: <input type="text" name="tipo" value="<?php echo $tipo; ?>" require>
-            <span class="error">* <?php echo $tipoErr; ?></span>
+            TIPO:
+            <input type="radio" name="tipo" value="Oficial" value="<?php echo $tipo; ?>" required>Oficial
+            <input type="radio" name="tipo" value="Libre" value="<?php echo $tipo; ?>" required>Libre 
+            <span class="error">*</span>
             <br><br>
-            INSCRIPCION: <input type="text" name="inscripcion" value="<?php echo $inscripcion; ?>" require>
-            <span class="error">* <?php echo $inscripcionErr; ?></span>
+            INSCRIPCION: <input type="date" name="inscripcion" value="<?php echo $inscripcion; ?>" max="<?php echo $inscripcion; ?>" required>
+            <span class="error">*</span>
             <br><br>
             <input type="submit" value="Guardar" name="guardar" />
         </form>
@@ -130,5 +80,4 @@
     }
     ?>
 </body>
-
 </html>
